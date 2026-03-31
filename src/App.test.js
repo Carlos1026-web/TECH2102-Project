cc5e7d165a075d402d8dbf5de3ca82adfa01becc
Carlos1026-web <Derek.carlos04@gmail.com>
2026-03-30 23:52:05 -0600

    Updated react app to show group number and group members

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders group information', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TECH 2102 - Group 13 - Derek, Kyrian, Renato/i);
   expect(linkElement).toBeInTheDocument();
 });
